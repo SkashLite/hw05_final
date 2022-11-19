beeaa50f87d3a2aa05a71779817cccaab0a34c92
Максим Виноградов <supermaks1999@gmail.com>
2022-11-19 19:35:30 +0300

    ...

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3-mdc*i&k1nu*-7&nm*6&_0900fa1(sf-z-_hwyq@c+g&i(2$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
